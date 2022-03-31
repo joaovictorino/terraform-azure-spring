4f8b6653c212beac3d9f50d3d474ad920f1944aa
joaovictorino <jhvictorino@gmail.com>
2022-03-30 23:17:05 -0300

    atualização do azurerm

--- provider.tf
+++ provider.tf
@@ -4,13 +4,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.25.0"
+      version = ">= 2.25.0"
     }
   }
 }
 
 provider "azurerm" {
-  skip_provider_registration = true
   features {}
 }
 
